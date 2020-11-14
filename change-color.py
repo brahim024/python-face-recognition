fa260f26787885364b3bf0bf0781acebd13e1024
brahim024 <ifninos168@gmail.com>
2020-11-14 17:18:06 +0100

    add some function to chnage color

---
+++ change-color.py
@@ -0,0 +1,9 @@
+import cv2
+img=cv2.imread('resource/lesly_zerna.png')
+imgGray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY )
+imgBlur=cv2.GaussianBlur(imgGray,(3,3),0)
+imgcanny=cv2.Canny(img,10,200)
+cv2.imshow('gray',imgGray)
+cv2.imshow('blur',imgBlur)
+cv2.imshow('canny',imgcanny)
+cv2.waitKey(0)
