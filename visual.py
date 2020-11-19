e8d69cffceb64c432d107d13798d377612beea2c
brahim024 <ifninos168@gmail.com>
2020-11-19 12:33:26 +0100

    add face tracking file

--- visual.py
+++ visual.py
@@ -3,7 +3,9 @@ import numpy as np
 kernel=np.ones((5,5),np.uint8)
 img=cv2.imread('resource/linus.jpg')
 imgcanny=cv2.Canny(img,5,100)
+imgeroded=cv2.erode(imgcanny,kernel,iterations=2)
+imgDialtion=cv2.dilate(img,kernel,iterations=2)
+cv2.imshow('dialation',imgDialtion)
+cv2.imshow('erod',imgeroded)
 
-imgDiaaltion=cv2.dilate(imgcanny,kernel,iterations=5)
-cv2.imshow('dialation',imgDiaaltion)
 cv2.waitKey(0)
