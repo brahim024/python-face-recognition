11df80ac804f37aa321f0f726412ac6eb3a4330e
brahim024 <ifninos168@gmail.com>
2020-11-27 23:29:09 +0100

    add face-rec files

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
-import cv2 
+'''import cv2 
 import numpy as np 
 img=cv2.imread('resource/joe-biden.jpg')
 print(img.shape)
 imgresise=cv2.resize(img,(600,600))
 cv2.imshow('Output',imgresise)
-cv2.waitKey(0) 
+cv2.waitKey(0)'''
+
+
